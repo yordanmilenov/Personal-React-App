20b1e996f666bbac7d5238f689db556f01cb7f1f
kiuchukparij <yordanmilenov777@gmail.com>
2023-03-11 09:49:59 +0200

    Added CRUD operations

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,71 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 import './App.css';
-
 function App() {
+  const [itemText, setItemText] = useState("");
+  const [listItems, setListItems] = useState([]);
+  const [isUpdating, setIsUpdating] = useState("");
+  const [updateItemText, setUpdateItemText] = useState("");
+
+  const addItem = async (e) => { 
+    e.preventDefault();
+    try {
+      const res = await axios.post("http://localhost:5500/api/item",{
+        item: itemText,
+      });
+      setListItems((prev) => [...prev, res.data]);
+      setItemText("");
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  useEffect(() => {
+    const getItemsList = async () => {
+      try {
+        const res = await axios.get("http://localhost:5500/api/items");
+        setListItems(res.data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    getItemsList();
+  }, []);
+
+  const deleteItem = async (id) => {
+    try {
+      const res = await axios.delete(
+        `http://localhost:5500/api/item/${id}`
+      );
+      const newListItems = listItems.filter((item) => item._id !== id);
+      setListItems(newListItems);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const updateItem = async (e) => { 
+    e.preventDefault();
+    try{
+      const res = await axios.put(
+        `http://localhost:5500/api/item/${isUpdating}`,
+        { item: updateItemText }
+      );
+      console.log(res.data);
+      const updateItemIndex = listItems.findIndex(
+        (item) => item._id === isUpdating
+      );
+      const updateItem = (listItems[updateItemIndex].item = updateItemText);
+      setUpdateItemText("");
+      setIsUpdating("");
+    }catch (error){
+      console.log(error);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    
+  )
 }
 
 export default App;
