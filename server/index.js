ecd01a6da4f9ac2aae2ccffbc1fc1c63151bad29
kiuchukparij <yordanmilenov777@gmail.com>
2023-03-11 08:36:06 +0200

    Connected to the server and added a model of the item

---
+++ server/index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv').config();
+const cors = require('cors');
+const TodoItemRoute = require('./routes/todoItems');
+const app = express();
+const PORT = 5500;
+
+app.use(express.json());
+app.use(cors());
+
+mongoose.connect('mongodb://localhost/todo')
+.then(() => console.log("Database connected"))
+.catch(err => console.log(err))
+
+app.use('/', TodoItemRoute);
+
+app.listen(PORT, () => console.log('Server connected'));
