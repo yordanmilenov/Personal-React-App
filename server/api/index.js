5632a1f95009ca5b67b7ba3307abcdf3e0080404
kiuchukparij <yordanmilenov777@gmail.com>
2023-03-20 14:05:14 +0200

    update

---
+++ server/api/index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv").config();
+const cors = require("cors");
+const TodoItemRoute = require("../routes/todoItems");
+const app = express();
+const PORT = process.env.PORT || 5500;
+const dbURL  = "mongodb+srv://yordanmilenov:0878884145@cluster0.bvf5cz1.mongodb.net/todos?retryWrites=true&w=majority";
+
+
+
+app.use(express.json());
+app.use(cors());
+
+mongoose
+    .connect(dbURL)
+    .catch((err) => console.log(err));
+
+app.use("/", TodoItemRoute);
+
+app.listen(PORT);
+
+
+module.exports = app;
